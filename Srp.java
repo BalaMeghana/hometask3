fdb79a81e1214cec7c27c656e81520370e93d0b8
BalaMeghana <60183450+BalaMeghana@users.noreply.github.com>
2020-02-10 17:56:18 +0530

    Add files via upload

---
+++ Srp.java
@@ -0,0 +1,9 @@
+package epamhometask3.solid;
+
+public class Srp {
+	public static void main(String[] args) {
+		MSrp m=new MSrp("Movie", 90);
+		System.out.print("The media is "+m.getName()+"Duration is"+m.getDuration());
+		
+	}
+}
